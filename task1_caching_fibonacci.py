0a45e53e6856af55d77faff3e46ad2bfb6bd41a4
Bohdan Shcherbak <b.szczerbak@gmail.com>
2026-03-04 14:54:48 +0100

    Fix fibonacci

--- task1_caching_fibonacci.py
+++ task1_caching_fibonacci.py
@@ -3,20 +3,18 @@ from typing import Callable
 def caching_fibonacci() -> Callable[[int], int]:
     """Returns a Fibonacci function that caches previously computed results."""
     
-    cache = []
+    cache = { 0: 0, 1: 1 }
 
     def fibonacci(n: int) -> int:
         """Calculates the nth Fibonacci number using caching."""
 
-        if n <= 0:
-            return 0
-        elif n == 1:
-            return 1
-        if n < len(cache):
+        if n < 0 or n != int(n):
+            raise ValueError("Input must be a non-negative integer.")
+        if n in cache:
             return cache[n]
         
         result = fibonacci(n - 1) + fibonacci(n - 2)
-        cache.append(result)
+        cache[n] = result
 
         return result
     
